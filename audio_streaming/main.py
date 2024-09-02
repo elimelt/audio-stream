a3108027eef559443238217da30f55ef72b94c7a
Elijah Melton <elimelt@uw.edu>
2024-09-02 14:25:06 -0700

    ssl

--- audio_streaming/main.py
+++ audio_streaming/main.py
@@ -9,7 +9,6 @@ def create_app():
     app = web.Application()
     app['config'] = Config()
     templates = os.path.join(app['config'].STATIC_ROOT, 'html')
-    print(templates)
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(templates))
     setup_routes(app)
     return app
--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 from audio_streaming.main import create_app
 from aiohttp import web
 from aiohttp.web import Application
+import ssl
 
 if __name__ == '__main__':
     app = create_app()
-    web.run_app(app, host=app['config'].HOST, port=app['config'].PORT)
+
+    # SSL context setup
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+
+    # Start the app with SSL
+    web.run_app(app, host='0.0.0.0', port=8080, ssl_context=ssl_context)
