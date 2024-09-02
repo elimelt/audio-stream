4a0895c7cf2876f6dc9a6bedef6b63b03ba8e250
Elijah Melton <elimelt@uw.edu>
2024-09-02 14:10:04 -0700

    change to ip

--- audio_streaming/config.py
+++ audio_streaming/config.py
@@ -3,7 +3,7 @@ import pathlib
 BASE_DIR = pathlib.Path(__file__).parent.parent
 
 class Config:
-    HOST = 'localhost'
+    HOST = '0.0.0.0'
     PORT = 8080
     BASE_DIR = BASE_DIR
     STATIC_ROOT = BASE_DIR / 'audio_streaming' / 'static'
